5bbc240d134f8d3e11630b46c27be364e4a45de3
jahiralamaumi <jahiralam25@gmail.com>
2021-07-07 21:49:18 +0600

    React playing

---
+++ test.js
@@ -0,0 +1,16 @@
+var a= "Habib";
+var b= 100.43;
+
+console.log(typeof(b));
+console.log(typeof(a));
+console.log(typeof(NaN));
+
+
+function sum(a,b){
+    var c= a+b;
+    return c;
+}
+
+console.log(sum(5, 6));
+
+/* js is a loosely typed language...*/
